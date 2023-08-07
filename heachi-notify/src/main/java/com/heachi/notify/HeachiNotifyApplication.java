1ba5b53fe218ca208a8a3ccfe471f48ee9bcbb08
ghdcksgml1 <ghdcksgml2@naver.com>
2023-08-07 21:22:15 +0900

    feat(#3): Webflux 사용을 위한 어노테이션 추가

--- heachi-notify/src/main/java/com/heachi/notify/HeachiNotifyApplication.java
+++ heachi-notify/src/main/java/com/heachi/notify/HeachiNotifyApplication.java
@@ -1,9 +1,15 @@
 package com.heachi.notify;
 
 import com.heachi.admin.common.utils.DateDistance;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
 public class HeachiNotifyApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(HeachiNotifyApplication.class, args);
+    }
 }
