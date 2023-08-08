abfd6b2b4f435fae4531371dd5cad70cb3de1c9f
ghdcksgml1 <ghdcksgml2@naver.com>
2023-08-08 12:57:55 +0900

    setting(#3): heachi-notify 모듈에서 Mongo 모듈을 스캔하기위해 basePackage 변경

--- heachi-notify/src/main/java/com/heachi/notify/HeachiNotifyApplication.java
+++ heachi-notify/src/main/java/com/heachi/notify/HeachiNotifyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.heachi")
 public class HeachiNotifyApplication {
 
     public static void main(String[] args) {
