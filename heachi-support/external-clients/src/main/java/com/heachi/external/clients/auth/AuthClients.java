4a78715bece8323e63ae765878d32647bcb7ae12
ghdcksgml1 <ghdcksgml2@naver.com>
2023-08-24 11:12:53 +0900

    feat(#39): 사용자 정보 가져오기 api

---
+++ heachi-support/external-clients/src/main/java/com/heachi/external/clients/auth/AuthClients.java
@@ -0,0 +1,15 @@
+package com.heachi.external.clients.auth;
+
+import com.heachi.admin.common.response.JsonResult;
+import com.heachi.external.annotation.ExternalClients;
+import com.heachi.external.clients.auth.response.UserInfoResponse;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.GetExchange;
+import reactor.core.publisher.Mono;
+
+@ExternalClients(baseUrl = "http://localhost:8080")
+public interface AuthClients {
+
+    @GetExchange(value = "/auth/info")
+    public Mono<JsonResult<UserInfoResponse>> getUserInfo(@RequestHeader(value = "Authorization") String headers);
+}
