5b277a7771f4a5939c7d4532a5818f2b6f34dd25
ghdcksgml1 <ghdcksgml2@naver.com>
2023-08-25 23:32:32 +0900

    test(#39): NotifyService의 30초동안 아무것도 안하면 커넥션 끊기는지 테스트 & 값을 넣으면 바로 데이터를 뱉어주는지 테스트

--- heachi-notify/src/test/java/com/heachi/notify/api/service/NotifyServiceTest.java
+++ heachi-notify/src/test/java/com/heachi/notify/api/service/NotifyServiceTest.java
@@ -1,24 +1,34 @@
 package com.heachi.notify.api.service;
 
+import com.heachi.admin.common.response.JsonResult;
 import com.heachi.mongo.define.notify.Notify;
+import com.heachi.mongo.define.notify.constant.NotifyType;
 import com.heachi.mongo.define.notify.repository.NotifyRepository;
 import com.heachi.notify.TestConfig;
-import com.heachi.notify.api.service.response.NotifyServiceReceiverResponse;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.heachi.notify.api.service.notify.NotifyService;
+import com.heachi.notify.api.service.notify.response.NotifyServiceReceiverResponse;
+import org.junit.jupiter.api.*;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest
 class NotifyServiceTest extends TestConfig {
@@ -29,36 +39,84 @@ class NotifyServiceTest extends TestConfig {
     @Autowired
     private NotifyService notifyService;
 
-    @AfterEach
-    void tearDown() {
-        notifyRepository.deleteAll();
+    @BeforeEach
+    void setUp() {
+        notifyRepository.deleteAll().block();
     }
 
     @Test
-    @Disabled
-    @DisplayName("구독 도중 객체가 추가되었을때 값이 반영이 되어있어야한다.")
-    void addNotifyWhenSubscribeOn() {
+    @DisplayName("구독하면, 30초가 커넥션이 유지되고, 30초가 지나면 커넥션은 끊긴다.")
+    void connection30secondsWhenClientSubscribe() {
         // given
-//        Notify notify1 = Notify.builder()
-//                .sendUserId("홍찬희1")
-//                .receiveUserIds(List.of("ghdcksgml1", "ghdcksgml2", "ghdcksgml3"))
-//                .build();
-//        Notify notify2 = Notify.builder()
-//                .sendUserId("홍찬희1")
-//                .receiveUserIds(List.of("ghdcksgml1", "ghdcksgml2", "ghdcksgml3"))
-//                .build();
-//
-//        // when
-//        Flux<NotifyServiceReceiverResponse> responseFlux = notifyService.receive("ghdcksgml1", 1);
-//        Flux<Notify> notifyFlux = notifyRepository.saveAll(List.of(notify1, notify2));
-//
-//        // then
-//        StepVerifier.create(responseFlux)
-//                .expectNextMatches(response -> {
-//                    assertThat(response.getSendUserId()).isEqualTo("홍찬희1");
-//
-//                    return true;
-//                })
-//                .verifyComplete();
+
+        // when
+        StepVerifier.withVirtualTime(() -> notifyService.receive("2954438047"))
+                // then
+                .expectSubscription()               // 구독이 시작되고 나서
+                .thenAwait(Duration.ofSeconds(30))  // 30초간 아무런 응답이 없으면
+                .expectNextMatches(jsonResult -> {  // 에러를 뱉음
+                    assertThat(jsonResult.getResCode()).isEqualTo(400);
+                    assertThat(jsonResult.getResMsg()).isEqualTo("Timeout");
+
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("구독 도중 객체가 추가되었을때 값이 곧바로 반영된다.")
+    void addNotifyWhenSubscribeOn() throws InterruptedException {
+        // given
+        Notify notify1 = Notify.builder()
+                .sendUserId("ghdcksgml1")
+                .receiveUserIds(List.of("ghdcksgml1", "ghdcksgml2", "ghdcksgml3"))
+                .type(NotifyType.NOTE)
+                .createdTime(LocalDateTime.now())
+                .checkedTime(new HashMap<>())
+                .checked(new HashSet<>())
+                .build();
+        Notify notify2 = Notify.builder()
+                .sendUserId("ghdcksgml2")
+                .receiveUserIds(List.of("ghdcksgml1", "ghdcksgml2", "ghdcksgml3"))
+                .type(NotifyType.NOTE)
+                .createdTime(LocalDateTime.now())
+                .checkedTime(new HashMap<>())
+                .checked(new HashSet<>())
+                .build();
+        Notify notify3 = Notify.builder()
+                .sendUserId("ghdcksgml3")
+                .receiveUserIds(List.of("ghdcksgml1", "ghdcksgml2", "ghdcksgml3"))
+                .type(NotifyType.NOTE)
+                .createdTime(LocalDateTime.now())
+                .checkedTime(new HashMap<>())
+                .checked(new HashSet<>())
+                .build();
+
+        notifyRepository.save(notify1).block(); // 매칭되는 아이디가 없으면 Flux가 실행이 안되기 때문에 임의의 데이터를 저장.
+
+        // when
+        StepVerifier.withVirtualTime(() -> {
+                    Flux<Notify> event = Mono.delay(Duration.ofSeconds(2)).thenMany(notifyRepository.saveAll(List.of(notify2, notify3)));
+                    Flux<JsonResult> flux = notifyService.receive("ghdcksgml1");
+
+                    return Flux.merge(event, flux).log();
+                })
+                // then
+                .expectSubscription()
+                .expectNextCount(1)                     // 임의의 데이터 1개
+                .expectNoEvent(Duration.ofSeconds(2))   // 2초뒤 2개 저장됨
+                .expectNextCount(4)                     // repository.save()가 2번 호출됨, notifyService.receive()에서 2개의 데이터를 바로 받음
+                .expectNoEvent(Duration.ofSeconds(30))  // idle 상태가 30초 동안 지속되면
+                .expectNextMatches(o -> {
+                    if (o instanceof JsonResult) {
+                        assertThat(((JsonResult<?>) o).getResCode()).isEqualTo(400);
+
+                        return true;
+                    }
+                    return false;
+                })
+                .verifyComplete();
+
+
     }
 }
