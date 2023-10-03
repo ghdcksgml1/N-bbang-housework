f2ea6c975e02d527ed6b32dc868d9cd8d5f64eee
ghdcksgml1 <ghdcksgml2@naver.com>
2023-10-03 19:46:10 +0900

    feat(#33): Common 모듈에 Exception 추가

--- heachi-support/common/src/main/java/com/heachi/admin/common/exception/s3/AwsS3Exception.java
+++ heachi-support/common/src/main/java/com/heachi/admin/common/exception/s3/AwsS3Exception.java
@@ -3,8 +3,8 @@ package com.heachi.admin.common.exception.s3;
 import com.heachi.admin.common.exception.ExceptionMessage;
 import com.heachi.admin.common.exception.HeachiException;
 
-public class S3FileSizeLimitExceededException extends HeachiException {
-    public S3FileSizeLimitExceededException(ExceptionMessage message) {
+public class AwsS3Exception extends HeachiException {
+    public AwsS3Exception(ExceptionMessage message) {
         super(message.getText());
     }
 }
