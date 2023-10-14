33e68b08d26713737ba0f49c1801a1fa13659fdc
jusung-c <anaooauc1236@naver.com>
2023-10-15 00:23:47 +0900

    feat(#69): HouseworkInfo Add API에 필요한 DTO 추가

---
+++ heachi-core/housework-api/src/main/java/com/heachi/housework/api/service/request/HouseworkServiceAddRequestDTO.java
@@ -0,0 +1,48 @@
+package com.heachi.housework.api.service.request;
+
+import com.heachi.housework.api.controller.request.HouseworkAddRequestDTO;
+import com.heachi.mysql.define.housework.category.HouseworkCategory;
+import com.heachi.mysql.define.housework.info.constant.HouseworkPeriodType;
+import com.heachi.mysql.define.housework.member.HouseworkMember;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HouseworkServiceAddRequestDTO {
+    private List<HouseworkMember> houseworkMembers;
+    private HouseworkCategory houseworkCategory;
+    private String title;
+    private String detail;
+    private HouseworkPeriodType type;
+    private Date dayDate;
+    private String weekDate;
+    private String monthDate;
+    private Date endTime;
+
+    public static HouseworkServiceAddRequestDTO of(HouseworkAddRequestDTO request) {
+        return HouseworkServiceAddRequestDTO.builder()
+                .houseworkMembers(request.getHouseworkMembers())
+                .houseworkCategory(request.getHouseworkCategory())
+                .title(request.getTitle())
+                .detail(request.getDetail())
+                .type(request.getType())
+                .dayDate(request.getDayDate())
+                .weekDate(request.getWeekDate())
+                .monthDate(request.getMonthDate())
+                .endTime(request.getEndTime())
+                .build();
+    }
+}
