702fb3827066741a778b8024bf026e32c7e471b3
ghdcksgml1 <ghdcksgml2@naver.com>
2023-10-21 22:53:58 +0900

    feat(#81): HOUSEWORK_PERIOD_DAY가 아닐때 dirtyBit Checking 로직 추가

--- heachi-domain-redis/src/main/java/com/heachi/redis/define/housework/todo/repository/TodoListRepository.java
+++ heachi-domain-redis/src/main/java/com/heachi/redis/define/housework/todo/repository/TodoListRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface TodoListRepository extends CrudRepository<TodoList, String> {
+
+    List<TodoList> findByGroupInfoId(@Param("groupInfoId") Long groupInfoId);
 
     Optional<TodoList> findByGroupInfoIdAndDate(@Param("groupInfoId") Long groupInfoId, @Param("date") LocalDate date);
 
