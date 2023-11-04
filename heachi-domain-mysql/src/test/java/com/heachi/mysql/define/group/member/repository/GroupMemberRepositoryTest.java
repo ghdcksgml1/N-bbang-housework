fea9bfb0f4cf27fe0b314ce8747685bae50a72a0
ghdcksgml1 <ghdcksgml2@naver.com>
2023-11-04 20:27:20 +0900

    test(#93): 쿼리 테스트

--- heachi-domain-mysql/src/test/java/com/heachi/mysql/define/group/member/repository/GroupMemberRepositoryTest.java
+++ heachi-domain-mysql/src/test/java/com/heachi/mysql/define/group/member/repository/GroupMemberRepositoryTest.java
@@ -6,6 +6,14 @@ import com.heachi.mysql.define.group.info.repository.GroupInfoRepository;
 import com.heachi.mysql.define.group.member.GroupMember;
 import com.heachi.mysql.define.group.member.constant.GroupMemberRole;
 import com.heachi.mysql.define.group.member.constant.GroupMemberStatus;
+import com.heachi.mysql.define.housework.category.HouseworkCategory;
+import com.heachi.mysql.define.housework.category.repository.HouseworkCategoryRepository;
+import com.heachi.mysql.define.housework.info.HouseworkInfo;
+import com.heachi.mysql.define.housework.info.repository.HouseworkInfoRepository;
+import com.heachi.mysql.define.housework.member.HouseworkMember;
+import com.heachi.mysql.define.housework.member.repository.HouseworkMemberRepository;
+import com.heachi.mysql.define.housework.todo.HouseworkTodo;
+import com.heachi.mysql.define.housework.todo.repository.HouseworkTodoRepository;
 import com.heachi.mysql.define.user.User;
 import com.heachi.mysql.define.user.constant.UserPlatformType;
 import com.heachi.mysql.define.user.constant.UserRole;
@@ -16,6 +24,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,8 +42,24 @@ class GroupMemberRepositoryTest extends TestConfig {
     @Autowired
     private GroupInfoRepository groupInfoRepository;
 
+    @Autowired
+    private HouseworkCategoryRepository houseworkCategoryRepository;
+
+    @Autowired
+    private HouseworkInfoRepository houseworkInfoRepository;
+
+    @Autowired
+    private HouseworkTodoRepository houseworkTodoRepository;
+
+    @Autowired
+    private HouseworkMemberRepository houseworkMemberRepository;
+
     @AfterEach
     void tearDown() {
+        houseworkTodoRepository.deleteAllInBatch();
+        houseworkMemberRepository.deleteAllInBatch();
+        houseworkInfoRepository.deleteAllInBatch();
+        houseworkCategoryRepository.deleteAllInBatch();
         groupMemberRepository.deleteAllInBatch();
         groupInfoRepository.deleteAllInBatch();
         userRepository.deleteAllInBatch();
@@ -52,5 +78,43 @@ class GroupMemberRepositoryTest extends TestConfig {
 
         // then
         assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Email과 todoId를 통해 GroupMember를 조회한다.")
+    void findGroupMemberByUserEmailAndTodoId() {
+        // given
+        User user = userRepository.save(generateUser());
+        User user2 = userRepository.save(generateCustomUser("ghdcksgml1@naver.com", "010-1111-1111"));
+        User user3 = userRepository.save(generateCustomUser("ghdcksgml2@naver.com", "010-2222-2222"));
+        GroupInfo groupInfo = groupInfoRepository.save(generateGroupInfo(user));
+        GroupInfo groupInfo2 = groupInfoRepository.save(generateGroupInfo(user2));
+        GroupInfo groupInfo3 = groupInfoRepository.save(generateGroupInfo(user3));
+        GroupMember groupMember = groupMemberRepository.save(generateGroupMember(user, groupInfo));
+        groupMemberRepository.save(generateGroupMember(user2, groupInfo));
+        groupMemberRepository.save(generateGroupMember(user3, groupInfo));
+        groupMemberRepository.save(generateGroupMember(user, groupInfo3));
+        groupMemberRepository.save(generateGroupMember(user3, groupInfo2));
+
+        HouseworkCategory houseworkCategory = houseworkCategoryRepository.save(generateHouseworkCategory());
+        HouseworkInfo houseworkInfo = houseworkInfoRepository.save(generateHouseworkInfo(houseworkCategory));
+        HouseworkInfo houseworkInfo2 = houseworkInfoRepository.save(generateHouseworkInfo(houseworkCategory));
+        HouseworkInfo houseworkInfo3 = houseworkInfoRepository.save(generateHouseworkInfo(houseworkCategory));
+
+        HouseworkMember houseworkMember = houseworkMemberRepository.save(generateHouseworkMember(groupMember, houseworkInfo));
+        HouseworkInfo findHouseworkInfo = houseworkInfoRepository.findHouseworkInfoByIdJoinFetchHouseworkMembers(houseworkInfo.getId()).get();
+        HouseworkInfo findHouseworkInfo2 = houseworkInfoRepository.findHouseworkInfoByIdJoinFetchHouseworkMembers(houseworkInfo2.getId()).get();
+        HouseworkInfo findHouseworkInfo3 = houseworkInfoRepository.findHouseworkInfoByIdJoinFetchHouseworkMembers(houseworkInfo3.getId()).get();
+
+        HouseworkTodo houseworkTodo = houseworkTodoRepository.save(generateHouseworkTodo(findHouseworkInfo, groupInfo, LocalDate.now()));
+        HouseworkTodo houseworkTodo2 = houseworkTodoRepository.save(generateHouseworkTodo(findHouseworkInfo2, groupInfo2, LocalDate.now()));
+        HouseworkTodo houseworkTodo3 = houseworkTodoRepository.save(generateHouseworkTodo(findHouseworkInfo3, groupInfo3, LocalDate.now()));
+
+        // when
+        GroupMember findGroupMember = groupMemberRepository.findGroupMemberByUserEmailAndTodoId(user.getEmail(), houseworkTodo.getId()).get();
+
+        // then
+        assertThat(findGroupMember.getId()).isEqualTo(groupMember.getId());
+        assertThat(findGroupMember.getUser().getId()).isEqualTo(user.getId());
     }
 }
