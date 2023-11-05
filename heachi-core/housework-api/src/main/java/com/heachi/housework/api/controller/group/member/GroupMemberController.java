f492515df6eae0f140027ac6095d6623a937e978
jusung-c <anaooauc1236@naver.com>
2023-11-05 19:48:43 +0900

    fix(#95): api 로직 수정

--- heachi-core/housework-api/src/main/java/com/heachi/housework/api/controller/group/member/GroupMemberController.java
+++ heachi-core/housework-api/src/main/java/com/heachi/housework/api/controller/group/member/GroupMemberController.java
@@ -6,6 +6,7 @@ import com.heachi.admin.common.response.JsonResult;
 import com.heachi.external.clients.auth.response.UserInfoResponse;
 import com.heachi.housework.api.controller.group.member.response.GroupMemberListResponse;
 import com.heachi.housework.api.service.auth.AuthExternalService;
+import com.heachi.housework.api.service.group.member.GroupMemberService;
 import com.heachi.mysql.define.group.member.repository.GroupMemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,7 @@ import java.util.List;
 @RequestMapping("/group")
 public class GroupMemberController {
     private final AuthExternalService authExternalService;
-    private final GroupMemberRepository groupMemberRepository;
+    private final GroupMemberService groupMemberService;
 
     @GetMapping("/member/{groupId}")
     public JsonResult<?> groupMemberList(@RequestHeader(name = "Authorization") String authorization,
@@ -27,21 +28,7 @@ public class GroupMemberController {
         // 유저 인증
         UserInfoResponse userInfoResponse = authExternalService.userAuthenticate(authorization);
 
-        // 권한 체크 - 해당 그룹의 그룹원인지 확인
-        boolean isGroupMember = groupMemberRepository.existsGroupMemberByUserEmailAndGroupInfoId(userInfoResponse.getEmail(), groupId);
-        if (!isGroupMember) {
-            log.warn(">>>> Is Not Group Member : {}", ExceptionMessage.GROUP_MEMBER_NOT_FOUND.getText());
-            throw new GroupMemberException(ExceptionMessage.GROUP_MEMBER_NOT_FOUND);
-        }
-
-        // groupId로 상태가 ACCEPT인 모든 그룹 멤버 조회 후 GroupMemberListResponse 리스트로 변환
-        List<GroupMemberListResponse> groupMemberList = groupMemberRepository.findGroupMemberByGroupId(groupId)
-                .stream()
-                .map(gm -> GroupMemberListResponse.builder()
-                        .groupMemberId(gm.getId())
-                        .username(gm.getUser().getName())
-                        .build())
-                .toList();
+        List<GroupMemberListResponse> groupMemberList = groupMemberService.selctGroupMember(userInfoResponse.getEmail(), groupId);
 
         return JsonResult.successOf(groupMemberList);
     }
---
+++ heachi-core/housework-api/src/main/java/com/heachi/housework/api/service/group/member/GroupMemberService.java
@@ -0,0 +1,40 @@
+package com.heachi.housework.api.service.group.member;
+
+import com.heachi.admin.common.exception.ExceptionMessage;
+import com.heachi.admin.common.exception.group.member.GroupMemberException;
+import com.heachi.housework.api.controller.group.member.response.GroupMemberListResponse;
+import com.heachi.mysql.define.group.member.repository.GroupMemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GroupMemberService {
+
+    private final GroupMemberRepository groupMemberRepository;
+
+    public List<GroupMemberListResponse> selctGroupMember(String email, Long groupId) {
+        // 권한 체크 - 해당 그룹의 그룹원인지 확인
+        boolean isGroupMember = groupMemberRepository.existsGroupMemberByUserEmailAndGroupInfoId(email, groupId);
+        if (!isGroupMember) {
+            log.warn(">>>> Is Not Group Member : {}", ExceptionMessage.GROUP_MEMBER_NOT_FOUND.getText());
+            throw new GroupMemberException(ExceptionMessage.GROUP_MEMBER_NOT_FOUND);
+        }
+
+        // groupId로 상태가 ACCEPT인 모든 그룹 멤버 조회 후 GroupMemberListResponse 리스트로 변환해서 반환
+
+        return groupMemberRepository.findGroupMemberByGroupId(groupId)
+                .stream()
+                .map(gm -> GroupMemberListResponse.builder()
+                        .groupMemberId(gm.getId())
+                        .username(gm.getUser().getName())
+                        .build())
+                .toList();
+    }
+}
